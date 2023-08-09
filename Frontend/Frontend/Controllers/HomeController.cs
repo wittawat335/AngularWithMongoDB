987b0ba62a07657a6b017563008c73ebae1b5f7b
Wittawat Fuangfu <wittawat335@gmail.com>
2023-08-09 21:39:58 +0700

    #

--- Frontend/Frontend/Controllers/HomeController.cs
+++ Frontend/Frontend/Controllers/HomeController.cs
@@ -18,6 +18,12 @@ namespace Frontend.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
         public IActionResult Privacy()
         {
             return View();
